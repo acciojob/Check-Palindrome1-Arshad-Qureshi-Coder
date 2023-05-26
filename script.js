304445c550c261a3e7dcfa917d9361d93cf0dc96
Arshad_Qureshi <130859896+Arshad-Qureshi-Coder@users.noreply.github.com>
2023-05-26 19:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+ let string = str.toLowerCase();
+	let ispalindrome= true;
+  let i=0,j=str.length()-1;
+	while (i <= j && ispalindrome) {
+		if (str[i]!==str[j]) {
+			ispalindrome=false;
+		}
+		i++;
+		j--;
+	}
+	return ispalindrome? true : false;
 }
 module.exports = palindrome
