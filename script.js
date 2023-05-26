c7ae05965dcbdbc843964e0df890538c9001898f
Arshad_Qureshi <130859896+Arshad-Qureshi-Coder@users.noreply.github.com>
2023-05-26 19:48:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 // complete the given function
 
 function palindrome(str){
- let string = str.toLowerCase();
-	let ispalindrome= true;
-  let i=0,j=str.length()-1;
-	while (i <= j && ispalindrome) {
-		if (str[i]!==str[j]) {
-			ispalindrome=false;
-		}
-		i++;
-		j--;
-	}
-	return ispalindrome? true : false;
+ // let string = str.toLowerCase();
+	// let ispalindrome= true;
+ //  let i=0,j=str.length()-1;
+	// while (i <= j && ispalindrome) {
+	// 	if (str[i]!==str[j]) {
+	// 		ispalindrome=false;
+	// 	}
+	// 	i++;
+	// 	j--;
+	// }
+	// return ispalindrome? true : false;
+	 const len = string.length;
+
+    // Use for loop to divide the words into 2 half
+    for (let i = 0; i < len / 2; i++) {
+
+        // validate the first and last characters are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'false';
+        }
+    }
+    return 'true';
 }
 module.exports = palindrome
